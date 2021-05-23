9bf1600c273007809e09405cfde63c3c0b4af2e1
Guillaume Cugnet <guillaume.cugnet@gmail.com>
2021-05-23 19:26:35 +0200

    [Unicolor] Change item type to RGB<u8>

--- src/sequence/unicolor.rs
+++ src/sequence/unicolor.rs
@@ -1,3 +1,5 @@
+use smart_leds::RGB;
+
 /// A sequence in which all LEDs have the same color.
 pub struct Unicolor<Color> {
     /// The color for all LEDs.
@@ -8,16 +10,13 @@ pub struct Unicolor<Color> {
     counter: usize,
 }
 
-impl<Color> Iterator for Unicolor<Color>
-where
-    Color: Copy,
-{
-    type Item = Color;
+impl<Color: Copy + Into<RGB<u8>>> Iterator for Unicolor<Color> {
+    type Item = RGB<u8>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.counter < self.number {
             self.counter += 1;
-            Some(self.color)
+            Some(self.color.into())
         } else {
             None
         }
