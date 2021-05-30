70ad27fa6713cc99c99e6ab56f1a057fde0bff56
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2021-05-30 11:22:47 +0200

    [sequence] Add enum-based dynamic dispatch

--- src/sequence.rs
+++ src/sequence.rs
@@ -8,10 +8,43 @@ pub use gradient::Gradient;
 pub use rainbow::Rainbow;
 pub use unicolor::Unicolor;
 
+use smart_leds::{hsv::Hsv, RGB8};
+
 /// A LED sequence.
 pub trait Sequence: Iterator {}
 
 /// A LED sequence with one parameter.
 pub trait OneParameterSequence<Color>: Sequence {
     fn new(color: Color, led_number: usize) -> Self;
+}
+
+/// Container enum for one-parameter sequences.
+pub enum OneParameterSequenceEnum {
+    Unicolor(Unicolor<Hsv>),
+    Rainbow(Rainbow),
+}
+
+impl From<Unicolor<Hsv>> for OneParameterSequenceEnum {
+    fn from(sequence: Unicolor<Hsv>) -> Self {
+        OneParameterSequenceEnum::Unicolor(sequence)
+    }
+}
+
+impl From<Rainbow> for OneParameterSequenceEnum {
+    fn from(sequence: Rainbow) -> Self {
+        OneParameterSequenceEnum::Rainbow(sequence)
+    }
+}
+
+impl Sequence for OneParameterSequenceEnum {}
+
+impl Iterator for OneParameterSequenceEnum {
+    type Item = RGB8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            OneParameterSequenceEnum::Unicolor(sequence) => sequence.next(),
+            OneParameterSequenceEnum::Rainbow(sequence) => sequence.next(),
+        }
+    }
 }
